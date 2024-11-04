bca3b425cc957d67b5e99b9caa93614f4c9d8328
Larissa Böhm Araújo <145381988+lalabohm@users.noreply.github.com>
2024-11-04 18:35:37 -0300

    Delete paineis_bi_workshop-master directory

--- paineis_bi_workshop-master/pd.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-dados = pd.read_csv('dados/dados_tic.csv')
-# dados['municipio'].head()
-linha = dados[dados['municipio']=='Planaltina'].head()
-print(type(linha), type(dados))
